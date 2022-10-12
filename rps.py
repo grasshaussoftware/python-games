e92f8362844d14685022bed6ba55490c1c559da7
deusopus <deusopus@gmail.com>
2022-10-12 04:07:16 -0700

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,15 +1,14 @@
-import random
-print("Rock, Paper, and Scissors by deusopus@gmail.com") 
+import random 
 class Game:
     def __init__(self, player1, opponent, choices):
         self.player1 = player1
         self.opponent = opponent
         self.choice = choices
 set = Game(input("Choose: "),random.choice([-4,-2,-1]),{"rock":4,"paper":2,"scissors":1})
-class Core:
+class Score:
     def __init__(self, outcome):
         self.outcome = outcome
-match = Core(set.choice[set.player1]+set.opponent)
+match = Score(set.choice[set.player1]+set.opponent)
 set.player1
 set.opponent
 if match.outcome == 0:
