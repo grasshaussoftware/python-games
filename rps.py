2c615ce5382af8fc164ca6b784c789640a1143df
deusopus <deusopus@gmail.com>
2022-10-11 23:10:49 -0700

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,28 +1,20 @@
 import random
-
+print("Rock, Paper, and Scissors by deusopus@gmail.com") 
 class Game:
     def __init__(self, player1, opponent, choices):
         self.player1 = player1
         self.opponent = opponent
         self.choice = choices
-
 set = Game(input("Choose: "),random.choice([-4,-2,-1]),{"rock":4,"paper":2,"scissors":1})
-
 class Core:
     def __init__(self, outcome):
         self.outcome = outcome
-
 match = Core(set.choice[set.player1]+set.opponent)
-
-print("Rock, Paper, and Scissors by deusopus@gmail.com")
-
-while match.outcome == 0:
-    set.player1
-    print(set.opponent)
-    if match.outcome == 0:
-        print("Tie")
-    elif match.outcome == 1 or 2 or -3:
-        print("Computer Wins")
-    elif match.outcome == 3 or -1 or -2:
-        print("You Win!")
-        
+set.player1
+set.opponent
+if match.outcome == 0:
+    print("Tie")
+elif match.outcome == 1 or 2 or -3:
+    print("Computer Wins")
+elif match.outcome == 3 or -1 or -2:
+    print("You Win!")
