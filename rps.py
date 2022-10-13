bd9598307f172b2b0195dc0d3561051bfa574df5
deusopus <deusopus@gmail.com>
2022-10-13 05:04:24 -0700

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,19 +1,22 @@
-import random 
-class Game:
-    def __init__(self, player1, opponent, choices):
-        self.player1 = player1
-        self.opponent = opponent
-        self.choice = choices
-set = Game(input("Choose: "),random.choice([-4,-2,-1]),{"rock":4,"paper":2,"scissors":1})
-class Score:
-    def __init__(self, outcome):
-        self.outcome = outcome
-match = Score(set.choice[set.player1]+set.opponent)
-set.player1
-set.opponent
-if match.outcome == 0:
-    print("Tie")
-elif match.outcome == 1 or 2 or -3:
-    print("Computer Wins")
-elif match.outcome == 3 or -1 or -2:
-    print("You Win!")
+import random
+print("Play until you win!") 
+print("Rock, Paper, or Scissors?")
+class GameEngine:
+    def __init__(self, computer):
+        self.computer = computer
+starter = GameEngine(random.choice([-4,-2,-1]))
+class Player:
+    def __init__ (self, options):
+        self.options = options
+play = Player({"rock":4,"paper":2,"scissors":1})
+class ScoreBoard:
+    def __init__(self, score, loss):
+        self.score = score
+        self.loss = loss
+while True:
+    match = ScoreBoard(play.options[input("Choose: ")]+starter.computer,[0,-1,-2,3])
+    if match.score in match.loss:
+        starter.computer = 0
+    else:
+        print("You win!")
+        break
